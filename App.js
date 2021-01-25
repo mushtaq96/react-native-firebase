e420b2600d088e94e19749b1c24c9f0604f3c13d
mushtaq96 <mushtaq96smb@gmail.com>
2021-01-26 00:12:35 +0900

    complete UI design

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { useState } from "react";
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from "react";
 import { HomeScreen, LoginScreen, RegistrationScreen } from "./src/screens";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         {user ? (
           <Stack.Screen name="Home">
-            {props => <HomeScreen {..props} extraData=[user]/>}
+            {props => <HomeScreen {...props} extraData={user} />}
           </Stack.Screen>
         ) : (
             <>
